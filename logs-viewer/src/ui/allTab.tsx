6f124e7c368c2568b77d785949d21ec1eaf2ce48
Dmitry Peshehonov <dyoma@tempo.io>
2023-09-12 04:46:40 +0300

    All tab: Filter by text

--- logs-viewer/src/ui/allTab.tsx
+++ logs-viewer/src/ui/allTab.tsx
@@ -62,12 +62,26 @@ export function AllEvents(props: {events: ObservableSet<LEvent>}) {
     }
   }, [filterLevels, filterPods]);
 
-  const filtered = props.events.useFilter(byGroupsFilter)
-  filtered.debugName = "FilteredByLevel+Group"
-  const snapshot = filtered.useSnapshot();
+  const filterPodsAndLevels = props.events.useFilter(byGroupsFilter)
+  filterPodsAndLevels.debugName = "FilteredByLevel+Group"
+
+  const [searchText, setSearchText] = React.useState("");
+  const textFilter = React.useMemo(() => {
+    return (e: LEvent) => {
+      return !searchText || e.data.message.indexOf(searchText) >= 0
+    }
+  }, [searchText]);
+  const filterText = filterPodsAndLevels.useFilter(textFilter);
+
+  const snapshot = filterText.useSnapshot();
   return <div className="ui-scroll-ancestor">
     <div className="ui-gr-option-pane">Levels:<Groups events={byLogLevel} selection={levels}/></div>
     <div className="ui-gr-option-pane">PODs:<Groups events={byPodName} selection={pods} groupOrder={Comparator.natural()}/></div>
+    <div className="ui-gr-text-search">
+      <span className="mr2">Filter by Text:</span>
+      <input type="text" placeholder="Search for substring in messages" value={searchText}
+             onChange={e => setSearchText(e.target.value)}/>
+    </div>
     <div className="ui-gr-bottom"/>
     <EventListComponent events={snapshot.sublist(0, 200)}/>
   </div>
