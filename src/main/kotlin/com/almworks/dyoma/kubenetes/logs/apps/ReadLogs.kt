b7027beb0ce1608be6add3191279b6b4edcff28b
Dmitry Peshehonov <dyoma@tempo.io>
2023-09-26 17:27:06 +0300

    Load exported log files

--- src/main/kotlin/com/almworks/dyoma/kubenetes/logs/apps/ReadLogs.kt
+++ src/main/kotlin/com/almworks/dyoma/kubenetes/logs/apps/ReadLogs.kt
@@ -6,12 +6,34 @@ import kotlinx.coroutines.runBlocking
 import java.io.File
 import java.io.FileInputStream
 import java.time.Instant
+import java.util.regex.Pattern
 
 fun main(): Unit = runBlocking {
   val server = Server.startDefault()
 
   val totalOrder = EventTotalOrder()
-  FileInputStream(File("/Users/dyoma/Downloads/logs-from-content-in-content-0 (32).log")).use { stream ->
-    ReceivedPodEvent.parsePodEventStream(PodInfo("XXX", Instant.now()), stream) { server.db.receiveEvent(totalOrder.assignSid(it)) }
+  val files = arrayListOf(
+    // Add your log files here
+    File("path/to/logs-from-POD-0 (3).log")
+  )
+  parseFiles(files, server, totalOrder)
+}
+
+private val FILE_NAME = Pattern.compile("(logs-from-)?([^ (]+).*")
+private suspend fun parseFiles(
+  files: ArrayList<File>,
+  server: Server,
+  totalOrder: EventTotalOrder
+) {
+  files.forEach { file ->
+    val m = FILE_NAME.matcher(file.name)
+    val podName = if (m.matches()) m.group(2)
+    else file.name.substringBefore(".", file.name)
+    FileInputStream(file).use { stream ->
+      ReceivedPodEvent.parsePodEventStream(
+        PodInfo(podName, Instant.now()),
+        stream
+      ) { server.db.receiveEvent(totalOrder.assignSid(it)) }
+    }
   }
 }
